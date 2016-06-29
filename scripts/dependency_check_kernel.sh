f897cd2b51e3ed21758aed4effe56815c1e18a1b
murray foster <mrafoster@gmail.com>
2016-06-29 01:16:01 -0700

    Fixed typo in script performing out kernel dependency check

--- scripts/dependency_check_kernel.sh
+++ scripts/dependency_check_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -d "norns-linux" ];
+if [ ! -d "norns-linux" ]; then
     git clone https://github.com/bonemurmurer/norns-linux.git
-    ln -s /home/pi/norns/norns-linux
+    ln -s /home/pi/norns/norns-linux ~/linux
     cp configs/norns_kernel_config norns-linux/.config
 fi
