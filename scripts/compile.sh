fbcf36ad64b3ee219a10734429787d5d9d7d22b7
murray foster <mrafoster@gmail.com>
2017-05-10 10:37:03 -0700

    Solving a typo in the compile arg..

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,5 +3,5 @@ cd monome-raspbian
 #create default configuration for this kernel version
 KERNEL=kernel7
 cp ../configs/kernel_config .config
-make -j4 ARCH=arm CROSS-COMPILE=arm-linux- zImage
+make -j4 ARCH=arm CROSS_COMPILE=arm-linux- zImage
 cd ..
