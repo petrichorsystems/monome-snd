cb444a95b85177f5f401d2a571b502e9133ec729
murray foster <mrafoster@gmail.com>
2017-05-11 12:51:43 -0700

    Fixed local (on arm/Raspi) compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,6 +3,5 @@ cd monome-raspbian
 #create default configuration for this kernel version
 KERNEL=kernel7
 cp ../configs/kernel_config .config
-MONOME_SND_COMPILE_EXIT=$(make -j4 ARCH=arm CROSS_COMPILE=arm-linux- zImage)
+make -j4 ARCH=arm CROSS_COMPILE=arm-linux- zImage
 cd ..
-exit $MONOME_SND_COMPILE_EXIT
